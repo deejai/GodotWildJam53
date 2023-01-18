db7672635197d732e92d2d038806d7163583b8ff
David Ferguson <deejai@live.com>
2023-01-17 21:26:29 -0700

    draggable boxes. world boundaries. started working on combiner

--- Main/Factory/Box.gd
+++ Main/Factory/Box.gd
@@ -1,13 +1,24 @@
 extends Node2D
 
+class_name ChuteBox
+
+var type: Main.BoxType = Main.BoxType.BLANK
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 func init(type: Main.BoxType):
+	self.type = type
 	$RigidBody2D/Sprite.frame = type
+
+func enable_gravity():
+	$RigidBody2D.gravity_scale = 1
+
+func disable_gravity():
+	$RigidBody2D.gravity_scale = 0
--- Main/Factory/Factory.gd
+++ Main/Factory/Factory.gd
@@ -3,8 +3,10 @@ extends Node2D
 var box = preload("res://Main/Factory/Box.tscn")
 
 var timer: float = 0
-var box_interval: float = 0.5
+var box_interval: float = 1.5
 var next_spawn: float = box_interval
+
+var grabbed_box: Node2D = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,7 +19,33 @@ func _process(delta):
 	if timer >= next_spawn:
 		next_spawn += box_interval
 		var new_box = box.instantiate()
-		new_box.init(1 + randi() % 6)
+		new_box.init([Main.BoxType.HEART, Main.BoxType.WATER, Main.BoxType.STAR][randi() % 3])
 		new_box.position.x = $BoxChute.position.x + $BoxChute.size.x/2 - 75 + randf() * 150
 		add_child(new_box)
 		print("box spawned at ", timer, " seconds")
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if grabbed_box == null and event.pressed:
+				var query := PhysicsPointQueryParameters2D.new()
+				query.position = event.position
+				var objects := get_world_2d().direct_space_state.intersect_point(query, 32)
+				for object in objects:
+					var node = objects[0].collider.get_owner()
+					if node is ChuteBox:
+						grabbed_box = node
+						grabbed_box.disable_gravity()
+						break
+			elif grabbed_box and !event.pressed:
+				grabbed_box.enable_gravity()
+				grabbed_box = null
+
+func _physics_process(delta):
+	if grabbed_box:
+		var body = grabbed_box.get_node("RigidBody2D")
+		var vec_to_mouse = get_global_mouse_position() - body.global_position
+		var vec_limit = 30
+		if vec_to_mouse.length() > vec_limit:
+			vec_to_mouse = vec_to_mouse.normalized() * vec_limit
+		body.linear_velocity = vec_to_mouse * 50
